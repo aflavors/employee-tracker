5da490eeaf965c0a72bb8355be76aa83752389bc
Akeem Flavors <akeemflavors@gmail.com>
2020-07-26 13:14:55 -0400

    employeeTrackerInit - inquirer outline

--- index.js
+++ index.js
@@ -21,3 +21,53 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
   connection.end();
 });
+
+//Application Initialization
+
+function employeeTrackerInit(){
+  inquirer
+    .prompt({
+      name: "main_prompt",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Departments",
+        "View All Roles",
+        "Add Employee",
+        "Add Role",
+        "Add Department",
+        "Update Employee Role",
+        "Exit Application"
+      ]
+    }).then(function(answer){
+      switch (answer.main_prompt){
+        case "View All Employees":
+          //viewEmployees function
+          break;
+        case "View All Departments":
+          //viewDepartments function
+          break;
+        case "View All Roles":
+          //viewRoles function
+          break;
+        case "Add Employee":
+          //addEmployee function
+          break;
+        case "Add Role":
+          //addRole function
+          break;
+        case "Add Department":
+          //addDepartment function
+          break;
+        case "Update Employee Role":
+          //updateRole function
+          break;
+        case "Exit Application":
+          connection.end();
+          break;
+      }
+    });
+}
+
+employeeTrackerInit();
